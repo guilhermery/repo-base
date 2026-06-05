e8457fd9fb4b3381d64fb5008b7a3c7bfc7600b4
guilherme2008 <guilhermepc2008@gmail.com>
2026-06-05 10:05:00 -0300

    Docs: Adicionando docsstrings para melhor entendimento do código

--- taskmanager/utils.py
+++ taskmanager/utils.py
@@ -8,7 +8,27 @@ def format_task(task):
     # Reordenando os elementos para o terceiro commit
     return f"#{task['id']} | {status} [{priority}] {task['title']}"
 
+"""
+    Formata uma tarefa para exibição.
+
+    Exibe o ID, status, prioridade e título da tarefa
+    em uma string formatada.
+    """
+
 def filter_tasks(tasks, show_done=True):
     if show_done:
         return tasks
     return [t for t in tasks if not t["done"]]
+
+
+"""
+    Filtra tarefas com base no status de conclusão.
+
+    Args:
+        tasks (list): Lista de tarefas.
+        show_done (bool): Se True, retorna todas as tarefas.
+                          Se False, retorna apenas as pendentes.
+
+    Returns:
+        list: Lista de tarefas filtradas.
+    """
